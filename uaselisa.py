b97b3f87d14364cbe4514830ad37313f5909189d
elisatri <129717320+elisatri@users.noreply.github.com>
2024-06-21 15:59:57 +0700

    Update uaselisa.py

--- uaselisa.py
+++ uaselisa.py
@@ -135,7 +135,7 @@ def fetch_data_product_prices(conn):
             factinternetsales fs
         INNER JOIN 
             dimproduct dp ON fs.productkey = dp.productkey
-            LIMIT 10000
+            LIMIT 5000
     """
     try:
         df = pd.read_sql(query, conn)
